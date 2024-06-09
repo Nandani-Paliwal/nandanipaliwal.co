038c3ff23ba950813dccae30fb183c68ba31cf7f
nandani <reach.nandanipaliwal@gmail.com>
2024-06-09 15:49:45 +0530

    fix: blog ui
    Signed-off-by: nandani <reach.nandanipaliwal@gmail.com>

--- src/Components/blog/modern-layout-posts.tsx
+++ src/Components/blog/modern-layout-posts.tsx
@@ -30,78 +30,83 @@ const PublicationPosts = (props: {
 	const slicedPosts = edges.map(edge => edge.node)
 
 	return (
-		<div className="blog-articles-area mx-auto mt-10 py-20  dark:border-slate-800 ">
-			<div className="flex flex-col gap-3 px-4 xl:px-20 2xl:px-24">
-				<h1 className="font-bold text-primary-500">Blogs</h1>
-				<div className="flex-col gap-3 md:flex">
-					<h2 className="max-w-3xl text-xl font-bold tracking-tighter md:text-5xl">
-						News, insights and more
-					</h2>
-					<p className="max-w-lg text-base font-normal tracking-tight md:text-xl">
-						Learn more about Clerk, our approach to authentication, and company news.
-					</p>
+		<div className="blog-articles-area  mx-auto mt-10 px-4 py-20 dark:border-slate-800">
+			<div className="absolute left-0 top-4  -z-0 h-px w-full border-t border-dashed border-zinc-400 py-5 dark:border-zinc-700 sm:top-6 md:top-[143px]"></div>
+			<div className="blog-articles-subarea border-x border-dashed border-zinc-400 py-5 dark:border-zinc-700">
+				<div className="relative flex flex-col gap-3">
+					{/* <div className="absolute left-0 top-0 -z-0 h-px w-full border-t border-dashed border-zinc-400 py-5 dark:border-zinc-700"></div> */}
+					<h1 className="font-bold text-primary-500">Blogs</h1>
+					<div className="flex-col gap-3 md:flex">
+						<h2 className="max-w-3xl text-xl font-bold tracking-tighter md:text-5xl">
+							News, insights and more
+						</h2>
+						<p className="max-w-lg text-base font-normal tracking-tight md:text-xl">
+							Learn more about Clerk, our approach to authentication, and company
+							news.
+						</p>
+					</div>
 				</div>
-			</div>
-			<div className="blog-articles-container container mx-auto flex w-full flex-col gap-10 px-4 py-4 md:grid md:grid-cols-2 lg:grid-cols-3 xl:px-20 xl:py-10 2xl:px-24 2xl:py-5">
-				{slicedPosts.map(post => (
-					<BlogPostPreview key={post.id} post={post} publication={publication} />
-				))}
-				{fetching && (
-					<>
-						<div className="col-span-1 animate-pulse">
-							<div
-								style={{ paddingTop: '52.5%' }}
-								className="mb-4 block w-full rounded-lg bg-slate-200 dark:border-slate-800"
-							/>
-							<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
-							<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
-							<div className="mb-2 h-4 w-2/3 rounded-lg bg-slate-200 dark:border-slate-800" />
-							<div className="mb-2 h-4 w-1/2 rounded-lg bg-slate-200 dark:border-slate-800" />
+				<div className="blog-articles-container 0 container mx-auto flex  w-full flex-col gap-10 pt-4 md:grid md:grid-cols-2 lg:grid-cols-3 xl:pt-10 2xl:pt-5">
+					{slicedPosts.map(post => (
+						<BlogPostPreview key={post.id} post={post} publication={publication} />
+					))}
+					{fetching && (
+						<>
+							<div className="col-span-1 animate-pulse">
+								<div
+									style={{ paddingTop: '52.5%' }}
+									className="mb-4 block w-full rounded-lg bg-slate-200 dark:border-slate-800"
+								/>
+								<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
+								<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
+								<div className="mb-2 h-4 w-2/3 rounded-lg bg-slate-200 dark:border-slate-800" />
+								<div className="mb-2 h-4 w-1/2 rounded-lg bg-slate-200 dark:border-slate-800" />
+							</div>
+							<div className="col-span-1 animate-pulse">
+								<div
+									style={{ paddingTop: '52.5%' }}
+									className="mb-4 block w-full rounded-lg bg-slate-200 dark:border-slate-800"
+								/>
+								<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
+								<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
+								<div className="mb-2 h-4 w-2/3 rounded-lg bg-slate-200 dark:border-slate-800" />
+								<div className="mb-2 h-4 w-1/2 rounded-lg bg-slate-200 dark:border-slate-800" />
+							</div>
+							<div className="col-span-1 animate-pulse">
+								<div
+									style={{ paddingTop: '52.5%' }}
+									className="mb-4 block w-full rounded-lg bg-slate-200 dark:border-slate-800"
+								/>
+								<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
+								<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
+								<div className="mb-2 h-4 w-2/3 rounded-lg bg-slate-200 dark:border-slate-800" />
+								<div className="mb-2 h-4 w-1/2 rounded-lg bg-slate-200 dark:border-slate-800" />
+							</div>
+						</>
+					)}
+					{pageInfo.hasNextPage && !fetching ? (
+						<div className="col-span-1 flex flex-row justify-center pt-10 md:col-span-2 lg:col-span-3">
+							<Button
+								type="button"
+								variant="outline"
+								className="w-full justify-center px-4 py-2 text-lg"
+								onClick={fetchMore}
+							>
+								<span>Load more</span>
+								<ChevronDownSVG className="ml-3 h-5 w-5 fill-current" />
+							</Button>
 						</div>
-						<div className="col-span-1 animate-pulse">
-							<div
-								style={{ paddingTop: '52.5%' }}
-								className="mb-4 block w-full rounded-lg bg-slate-200 dark:border-slate-800"
-							/>
-							<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
-							<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
-							<div className="mb-2 h-4 w-2/3 rounded-lg bg-slate-200 dark:border-slate-800" />
-							<div className="mb-2 h-4 w-1/2 rounded-lg bg-slate-200 dark:border-slate-800" />
-						</div>
-						<div className="col-span-1 animate-pulse">
-							<div
-								style={{ paddingTop: '52.5%' }}
-								className="mb-4 block w-full rounded-lg bg-slate-200 dark:border-slate-800"
-							/>
-							<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
-							<div className="mb-2 h-4 rounded-lg bg-slate-200 dark:border-slate-800" />
-							<div className="mb-2 h-4 w-2/3 rounded-lg bg-slate-200 dark:border-slate-800" />
-							<div className="mb-2 h-4 w-1/2 rounded-lg bg-slate-200 dark:border-slate-800" />
-						</div>
-					</>
-				)}
-				{pageInfo.hasNextPage && !fetching ? (
-					<div className="col-span-1 flex flex-row justify-center py-10 md:col-span-2 lg:col-span-3">
-						<Button
-							type="button"
-							variant="outline"
-							className="w-full justify-center px-4 py-2 text-lg"
-							onClick={fetchMore}
-						>
-							<span>Load more</span>
-							<ChevronDownSVG className="ml-3 h-5 w-5 fill-current" />
-						</Button>
+					) : null}
+				</div>
+				{/* {fetchedOnce && pageInfo.hasNextPage ? (
+				<Waypoint onEnter={fetchMore} topOffset="-20%" />
+			) : null} */}
+				{fetchedOnce && !pageInfo.hasNextPage ? (
+					<div className="blog-posts-end-card font-heading mt-10 px-16 pt-8 text-center font-bold text-slate-700 dark:text-slate-300">
+						<p className="text-2xl">You&apos;ve reached the end! 👋</p>
 					</div>
 				) : null}
 			</div>
-			{/* {fetchedOnce && pageInfo.hasNextPage ? (
-				<Waypoint onEnter={fetchMore} topOffset="-20%" />
-			) : null} */}
-			{fetchedOnce && !pageInfo.hasNextPage ? (
-				<div className="blog-posts-end-card font-heading mt-10 px-16 py-8 text-center font-bold text-slate-700 dark:text-slate-300">
-					<p className="text-2xl">You&apos;ve reached the end! 👋</p>
-				</div>
-			) : null}
 		</div>
 	)
 }
