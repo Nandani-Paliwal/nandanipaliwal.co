b0db36f2658e3b1c3d9a89609accbc3afd53c617
nandani <reach.nandanipaliwal@gmail.com>
2024-06-09 11:18:31 +0530

    fix: meta description
    Signed-off-by: nandani <reach.nandanipaliwal@gmail.com>

--- src/constant.ts
+++ src/constant.ts
@@ -4,4 +4,4 @@ export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
 export const IS_STAGING = !IS_DEVELOPMENT && !IS_PRODUCTION
 
 export const siteDescription =
-	"Specializing in backend engineering and DevOps, I'm dedicated to transforming core ideas into high-performance digital realities.Streamlining development processes and building robust, scalable solutions.Let’s drive innovation together! 🚀"
+	"Specializing in frontend engineering and animations, I'm dedicated to transforming core ideas into high-performance digital realities.Streamlining development processes and building robust, scalable solutions.Let’s drive innovation together! 🚀"
