7cab34c10e11fdcb3e229af88245638555202e47
nandani <reach.nandanipaliwal@gmail.com>
2024-06-23 07:58:06 +0530

    fix: minor fixes
    Signed-off-by: nandani <reach.nandanipaliwal@gmail.com>

--- src/Components/navbar/nav-links.tsx
+++ src/Components/navbar/nav-links.tsx
@@ -5,7 +5,10 @@ import { AnimatePresence, motion } from 'framer-motion'
 const NavLinks = () => {
 	const [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
 
-	const navigation = [{ label: 'Blogs', href: '/blog' }]
+	const navigation = [
+		{ label: 'Blogs', href: '/blog' },
+		{ label: 'Resume', href: '/resume.pdf' }
+	]
 
 	return (
 		<>
@@ -13,6 +16,7 @@ const NavLinks = () => {
 				<Link
 					key={item.label}
 					href={item.href}
+					target="_blank"
 					className="relative -mx-3 -my-2 rounded-lg px-3 py-2 text-sm font-medium text-slate-700 transition-colors delay-150 hover:text-slate-500 hover:delay-[0ms] dark:text-secondary-400  dark:hover:text-secondary-500"
 					onMouseEnter={() => setHoveredIndex(index)}
 					onMouseLeave={() => setHoveredIndex(null)}
